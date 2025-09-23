6cc8f21f73b484e63b359f0a4ceb430059118abe
gitSomchai sabaiJaidee JA <Aemamorn.su@ku.th>
2025-09-23 14:58:41 +0700

    2864

--- assignments/week09/random_quiz-1.py
+++ assignments/week09/random_quiz-1.py
@@ -25,3 +25,26 @@ Example
     Congratulations! You won in 3 attempts!
 
 """
+import random
+
+print("=== SIMPLE GUESSING GAME ===")
+print("Guessma number between 1 and 20!")
+print("You have 6 atttempts\n")
+    #randomnumber
+random_number = random.randint(1, 20)
+
+
+for i in range(6):
+    guess_number = int(input(f"Attemp {i+1}/6 - Enter your guess: "))
+
+    if random_number == guess_number:
+        print(f"Congrattulations! You won in {i+1} attempts")
+        break
+
+    elif random_number < guess_number:
+        print("Too much")
+
+    else:
+        print("Too low")
+
+print(random_number)
